cd8a79c46070e249617772e5c197f532c50f028f
marsila <eng.rasha.ali7@gmail.com>
2026-06-11 13:56:43 +0200

    Add validation to the blog creation form

--- blog-list/app/actions/blogs.ts
+++ blog-list/app/actions/blogs.ts
@@ -4,14 +4,23 @@ import { revalidatePath } from 'next/cache'
 import { addBlog, updateBlog } from '../services/blogs'
 import { auth } from '../auth'
 
-export const createBlog = async( formData :FormData) => {
+export const createBlog = async( prevState: {error:string},formData :FormData) => {
     const session = await auth()
     if(!session){
         return redirect('/login')
     }
     const title = formData.get('title') as string
+    if(!title || title.length < 5){
+        return{error : "Title must be at least 5 character long"}
+    }
     const author = formData.get('author') as string
+    if(!author || author.length < 5){
+        return{error : "Author must be at least 5 character long"}
+    }
     const url = formData.get('url') as string
+    if(!url || url.length < 5){
+        return{error : "URL must be at least 5 character long"}
+    }
     await addBlog(title,author,url)
     revalidatePath('/blogs')
     redirect('/blogs')
--- blog-list/app/blogs/new/page.tsx
+++ blog-list/app/blogs/new/page.tsx
@@ -1,27 +1,33 @@
+"use client"
+import { useActionState } from "react";
 import { createBlog } from "@/app/actions/blogs";
 
 export default function NewBlog(){
+    const [state, formAction] = useActionState(createBlog,{error:''})
     return(
         <div>
             <h1>Create a new blog</h1>
-            <form action={createBlog}>
+            <form action={formAction}>
                 <div>
                     <label htmlFor="title">
-                        Title <input type="text" name="title" id="title" />
+                        Title <input type="text" name="title" id="title" minLength={5} required/>
                     </label>
                 </div>
                 <div>
                     <label htmlFor="author">
-                        Author <input type="text" name="author" id="author" />
+                        Author <input type="text" name="author" id="author"  minLength={5} required/>
                     </label>
                 </div>
                 <div>
                     <label htmlFor="url">
-                        URL <input type="text" name="url" id="url" />
+                        URL <input type="text" name="url" id="url" minLength={5} required/>
                     </label>
                 </div>
                 <div>
                     <button type="submit">Create</button>
+                </div>
+                <div>
+                    {state.error && <p style={{color:'red'}}>{state.error}</p>}
                 </div>
             </form>
         </div>
