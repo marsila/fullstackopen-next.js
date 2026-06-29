5dc6b9a6c280ffc18ed6373c58e083fff7214ad7
marsila <eng.rasha.ali7@gmail.com>
2026-06-29 16:39:42 +0200

    add configuration to playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,4 +15,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
+  webServer: {
+    command: 'npm run build && npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
